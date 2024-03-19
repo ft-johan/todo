c3db747285fe63c503fa4ee12938ae407e741c74
Johan george <johangeorge201203@gmail.com>
2024-03-19 20:20:37 +0530

    hero

--- todo/app/layout.tsx
+++ todo/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- todo/app/page.tsx
+++ todo/app/page.tsx
@@ -1,21 +1,38 @@
-import Image from "next/image";
-import { SparklesCore } from "./ui/sparkles";
 
+"use client";
+import { SparklesCore } from "./ui/sparkles";
+import { LampContainer } from "./ui/lamp";
+import { motion } from "framer-motion";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div className="w-full absolute inset-0 h-screen">
+    <main>
+
         <SparklesCore
           id="tsparticlesfullpage"
           background="transparent"
           minSize={0.5}
           maxSize={1.0}
           particleDensity={50}
-          className="w-full h-full"
+          className="w-full h-full absolute z-50"
           particleColor="#FFFFFF"
         />
-      </div>
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl relative z-0"
+          >
+            Mind <br /> dump
+          </motion.h1>
+        </LampContainer>
+ 
     </main>
   );
 }
