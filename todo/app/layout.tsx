b68ab18459f485ffae0ddf707e0072d007020d03
Johan george <johangeorge201203@gmail.com>
2024-03-19 21:03:16 +0530

    skel

--- todo/app/layout.tsx
+++ todo/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 
-const inter = Raleway({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- todo/app/page.tsx
+++ todo/app/page.tsx
@@ -4,6 +4,16 @@ import { SparklesCore } from "./ui/sparkles";
 import { LampContainer } from "./ui/lamp";
 import { motion } from "framer-motion";
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
 
 export default function Home() {
   return (
@@ -18,21 +28,67 @@ export default function Home() {
           className="w-full h-full absolute z-50"
           particleColor="#FFFFFF"
         />
-        <LampContainer>
-          <motion.h1
-            initial={{ opacity: 0.5, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl relative z-0"
-          >
-            Mind <br /> dump
-          </motion.h1>
-        </LampContainer>
- 
+       <h1 className=" mt-8 md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+        Mind
+      </h1>
+      <BentoGrid className="mt-25 mx-auto">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          tag={item.tag}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
     </main>
   );
 }
+
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    tag: "Explore the birth of groundbreaking ideas and inventions.",
+   
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Digital Revolution",
+    tag: "Dive into the transformative power of technology.",
+
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    tag: "Discover the beauty of thoughtful and functional design.",
+
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Power of Communication",
+    tag:
+      "Understand the impact of effective communication in our lives.",
+
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Pursuit of Knowledge",
+    tag: "Join the quest for understanding and enlightenment.",
+
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Joy of Creation",
+    tag: "Experience the thrill of bringing ideas to life.",
+
+    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Spirit of Adventure",
+    tag: "Embark on exciting journeys and thrilling discoveries.",
+
+    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+];
